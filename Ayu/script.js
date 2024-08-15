bd65cf37a95bebaf5c142feb6c5475ab25986a2b
FTFSTORE <ftfstore.pagamentos@gmail.com>
2024-08-14 22:28:32 -0300

    Delete Ayu directory

--- Ayu/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Aqui você pode adicionar qualquer funcionalidade adicional após o carregamento
-    // Por exemplo, redirecionar para outra página ou ocultar a tela de carregamento
-    setTimeout(function() {
-        document.getElementById("loading-screen").style.display = "none";
-    }, 8000); // Tempo simulado de carregamento de 5 segundos
-});
